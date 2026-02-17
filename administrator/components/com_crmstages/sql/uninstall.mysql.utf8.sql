e0a9396c039d843b633c76442057ca4d4f7e943d
Svetlana Kartysh <ra@5rm.ru>
2026-02-17 11:56:18 +0300

    rm redundant

--- administrator/components/com_crmstages/sql/uninstall.mysql.utf8.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS `#__crm_action_log`;
-DROP TABLE IF EXISTS `#__crm_stage_actions`;
-DROP TABLE IF EXISTS `#__crm_companies`;
-DROP TABLE IF EXISTS `#__crm_stages`;
-DROP TABLE IF EXISTS `#__crm_actions`;
-
--- media/plg_system_jooa11y/js/jooa11y.js
+++
@@ -1,7 +0,0 @@
-import { Lang, Sa11y } from 'sa11y';
-import Sa11yLang from 'sa11y-lang';
-
-Lang.addI18n(Sa11yLang.strings);
-window.addEventListener('load', () => {
-  new Sa11y(Joomla.getOptions('jooa11yOptions', {}));
-});
